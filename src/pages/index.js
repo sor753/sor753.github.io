5506a5008572c2be25a935d38aa29b54b376c48c
show kawazoe <show753@icloud.com>
2022-05-09 13:22:41 +0900

    set CSS modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,24 @@
 import * as React from "react"
+import * as classes from "./common.module.scss"
 
 const IndexPage = () => (
-  <div>Hello</div>
+  <>
+    <header className="header">
+      <div className="container">
+        <div className="site">
+        </div>
+        <nav className="nav">
+          <ul>
+            <li>TOP</li>
+            <li>ABOUT</li>
+            <li>LIST</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <section className="hero"></section>
+    <footer className="footer"></footer>
+  </>
 )
 
 export default IndexPage
